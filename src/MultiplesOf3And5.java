870be98c0596f80bc67c5f823cc31f608c52f6b4
xoften <mj223gn@student.lnu.se>
2016-04-05 18:41:53 +0200

    Initial commit

---
+++ src/MultiplesOf3And5.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Michael Johansson(mj223gn) on 2016-04-05.
+ */
+public class MultiplesOf3And5 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+    }
+
+}
